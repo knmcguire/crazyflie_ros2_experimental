6265a4fddddf9b002079e4ff21957308fa6d3086
knmcguire <kimberly@bitcraze.io>
2022-05-20 16:02:15 +0200

    add cflib as external dependency and try out log

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # crazyflie_ros2
+
+For installing cflib as an external dependency
+
+```
+sudo mkdir -p /usr/local/share/ros && sudo cp cflib-python.yaml /usr/local/share/ros
+```
+
+```
+echo "yaml file:///usr/local/share/ros/cflib-python.yaml" | sudo tee -a /etc/ros/rosdep/sources.list.d/20-default.list
+rosdep update
+```
+```
+rosdep install --from-paths src --ignore-src -r -y 
+```
--- crazyflie_ros2/crazyflie_ros2/crazyflie_publisher.py
+++ crazyflie_ros2/crazyflie_ros2/crazyflie_publisher.py
@@ -2,17 +2,33 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+import cflib.crtp  # noqa
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+from cflib.utils import uri_helper
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 
+uri = uri_helper.uri_from_env(default='radio://0/40/2M/E7E7E7E703')
 
 class CrazyfliePublisher(Node):
 
-    def __init__(self):
+
+
+    def __init__(self, link_uri):
         super().__init__('crazyflie_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        #self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
+        self._cf = Crazyflie(rw_cache='./cache')
+        self._cf.connected.add_callback(self._connected)
+        self._cf.disconnected.add_callback(self._disconnected)
+        self._cf.connection_failed.add_callback(self._connection_failed)
+        self._cf.connection_lost.add_callback(self._connection_lost)
+        self._cf.open_link(link_uri)
+
+    
     def timer_callback(self):
         msg = String()
         msg.data = 'Hello World: %d' % self.i
@@ -20,11 +36,58 @@ class CrazyfliePublisher(Node):
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
 
+    def _connected(self, link_uri):
+        print('connected')
+        self._lg_stab = LogConfig(name='Stabilizer', period_in_ms=100)
+        self._lg_stab.add_variable('stateEstimate.x', 'float')
+        self._lg_stab.add_variable('stateEstimate.y', 'float')
+        self._lg_stab.add_variable('stateEstimate.z', 'float')
+        self._lg_stab.add_variable('stabilizer.roll', 'float')
+        self._lg_stab.add_variable('stabilizer.pitch', 'float')
+        self._lg_stab.add_variable('stabilizer.yaw', 'float')
+
+        try:
+            self._cf.log.add_config(self._lg_stab)
+            # This callback will receive the data
+            self._lg_stab.data_received_cb.add_callback(self._stab_log_data)
+            # This callback will be called on errors
+            self._lg_stab.error_cb.add_callback(self._stab_log_error)
+            # Start the logging
+            self._lg_stab.start()
+        except KeyError as e:
+            print('Could not start log configuration,'
+                  '{} not found in TOC'.format(str(e)))
+        except AttributeError:
+            print('Could not add Stabilizer log config, bad configuration.')
+
+
+    def _disconnected():
+        print('disconnected')
+
+    def _connection_failed(self, link_uri, msg):
+        print('connection_failed')
+
+    def _connection_lost(self, link_uri, msg):
+        print('connection_lost')
+
+    def _stab_log_error(self, logconf, msg):
+        """Callback from the log API when an error occurs"""
+        print('Error when logging %s: %s' % (logconf.name, msg))
+
+    def _stab_log_data(self, timestamp, data, logconf):
+        """Callback from a the log API when data arrives"""
+        print(f'[{timestamp}][{logconf.name}]: ', end='')
+        for name, value in data.items():
+            print(f'{name}: {value:3.3f} ', end='')
+        print()
 
 def main(args=None):
+
+    cflib.crtp.init_drivers()
+
     rclpy.init(args=args)
 
-    crazyflie_publisher = CrazyfliePublisher()
+    crazyflie_publisher = CrazyfliePublisher(uri)
 
     rclpy.spin(crazyflie_publisher)
 
@@ -32,5 +95,10 @@ def main(args=None):
     rclpy.shutdown()
 
 
+
 if __name__ == '__main__':
     main()
+
+
+
+
