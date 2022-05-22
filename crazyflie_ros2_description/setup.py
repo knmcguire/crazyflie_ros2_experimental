1abecaa97e2bd24b3475955d692923438bf77ed6
knmcguire <kimberly@bitcraze.io>
2022-05-22 13:18:28 +0200

    fix rviz copy colcon build

--- crazyflie_ros2_description/setup.py
+++ crazyflie_ros2_description/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
