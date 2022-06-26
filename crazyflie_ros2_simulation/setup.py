721cd43b6f8132ad3aebb02409ce9dfa08175e02
knmcguire <kimberly@bitcraze.io>
2022-06-26 17:02:11 +0200

    add launch file with urdf, add datafiles to setup

--- crazyflie_ros2_simulation/setup.py
+++ crazyflie_ros2_simulation/setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup
 
 package_name = 'crazyflie_ros2_simulation'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/my_world.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.urdf']))
+data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='knmcguire',
