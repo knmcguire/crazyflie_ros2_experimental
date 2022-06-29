9cce76c346726cbfaf19386cc45de4d7c33a57b7
knmcguire <kimberly@bitcraze.io>
2022-06-29 11:05:47 +0200

    Started simple mapper package

---
+++ crazyflie_ros2_simple_mapper/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'crazyflie_ros2_simple_mapper'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='knmcguire',
+    maintainer_email='kimberly@bitcraze.io',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'simple_mapper = crazyflie_ros2_simple_mapper.simple_mapper:main'
+        ],
+    },
+)
